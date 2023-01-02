3751bafefc8b31ba121d9ab5a42f34fe11beb22c
Victoria Rivas <victoriamagalirivas@gmail.com>
2023-01-02 00:02:17 -0300

    Primer escritura del repositorio

--- Bloque Java/js/ejemplo.js
+++ Bloque Java/js/ejemplo.js
@@ -1,5 +1,4 @@
-
-document.getElementById("boton").onclick = function () {
+document.getElementById('boton').onclick = function() {
     console.log("Capturamos el evento click");
     document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en js";
 }
